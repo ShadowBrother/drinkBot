9daded9ecba09a7aa843fda8de08f150b9762a37
ShadowBrother <jesselhoyt@gmail.com>
2014-05-31 04:34:11 -0400

    db_tools and website updates
    -updated db_tools functions to handle input of strings or ints so for
    example you can call list_recipe giving either the drink name, or the
    drink's id
    -fixed bug in have_ingredient
    -added get_drink_id
    -updated index.cgi and drinkBot.js to include order button which at the
    moment shows popup indicating the id number of the drink you ordered

--- db_tools.py
+++ db_tools.py
@@ -6,15 +6,22 @@
 """list_ingredients
    returns: a list of tuples of ingredients needed to make a drink
    cur(obj) : db cursor object
-   drink_name(string) : name of drink
+   drink(string or int) : name of drink or db id of drink
 """
-def list_ingredients(cur, drink_name):
+def list_ingredients(cur, drink):
     result = []
-    cur.execute(("SELECT Liquids.name FROM Liquids " 
-    "JOIN Drinks "
-    "JOIN Mix "
-    "WHERE Drinks.name == ? AND Drinks.id == Mix.drink_id AND Liquids.id == Mix.liquid_id"), (drink_name,))
-    for row in cur:
+
+    if str(drink).isdigit():#check if drink is int
+        
+        cur.execute(("SELECT Liquids.name FROM Liquids "
+                     "JOIN Mix "
+                     "WHERE Mix.drink_id = ? AND Mix.liquid_id == Liquids.id"), (drink,))
+    else:#drink is string 
+        cur.execute(("SELECT Liquids.name FROM Liquids " 
+                     "JOIN Drinks "
+                     "JOIN Mix "
+                     "WHERE Drinks.name == ? AND Drinks.id == Mix.drink_id AND Liquids.id == Mix.liquid_id"), (drink,))
+    for row in cur:#fetch query results and append to a list
         result.append(row)
     return result
 
@@ -22,16 +29,18 @@ def list_ingredients(cur, drink_name):
 """have_ingredients
    returns(boolean) : True if have all needed ingredients, False if missing any ingredients
    cur(obj) : db cusor object
-   drink_name(string) : name of drink
+   drink(string or int) : name of drink or db id of drink
 """
-def have_ingredients(cur, drink_name):
-    
-    #print drink_name
+def have_ingredients(cur, drink):
     
     
-    cur.execute("SELECT COUNT(*) FROM Mix WHERE drink_id == (SELECT id FROM Drinks WHERE name == ?) AND NOT EXISTS (SELECT id FROM Inventory WHERE Inventory.liquid_id == Mix.liquid_id)", (drink_name,))
+    
+    if str(drink).isdigit():#if drink is int
+        cur.execute("SELECT COUNT(*) FROM Mix WHERE drink_id == ? AND NOT EXISTS (SELECT id FROM Inventory WHERE Inventory.liquid_id == Mix.liquid_id)", (drink,))
+    else:#drink is string            
+        cur.execute("SELECT COUNT(*) FROM Mix WHERE drink_id == (SELECT id FROM Drinks WHERE name == ?) AND NOT EXISTS (SELECT id FROM Inventory WHERE Inventory.liquid_id == Mix.liquid_id)", (drink,))
     missing = cur.fetchone()[0] #untuple it
-    #print missing
+    
     if missing == 0:
         return True
     else:
@@ -40,26 +49,38 @@ def have_ingredients(cur, drink_name):
 """have_ingredient
    returns(boolean) : True if have ingredient, False if missing ingredient
    cur(obj) : db cursor object
-   ingredient_name(string) : name of ingredient
+   ingredient(string or int) : name of ingredient or db id for ingredient
 """
-def have_ingredient(cur, ingredient_name):
+def have_ingredient(cur, ingredient):
     
-    cur.execute(("SELECT CASE WHEN EXISTS "
-                 "(SELECT Liquids.name FROM Liquids JOIN Inventory WHERE Liquids.name == ? AND Liquids.id == Inventory.id) "
-                 "THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END"), (ingredient_name,))
+    if str(ingredient).isdigit():#if drink is int
+        cur.execute(("SELECT CASE WHEN EXISTS "
+                     "(SELECT * FROM Inventory WHERE liquid_id == ?) "
+                     "THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END"), (ingredient,))
+    else:#drink is string
+        cur.execute(("SELECT CASE WHEN EXISTS "
+                     "(SELECT Liquids.name FROM Liquids JOIN Inventory "
+                     "WHERE Liquids.name == ? AND Liquids.id == Inventory.liquid_id) "
+                     "THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END"), (ingredient,))
     return cur.fetchone()[0]
     
 
 """list_recipe
    returns(list of tuples) : a list of tupples of ingredients and amount to make a drink
    cur(obj) : db cursor object
-   drink_name(string) : name of drink
+   drink(string or int) : name of drink or db id for drink
 """
-def list_recipe(cur, drink_name):
+def list_recipe(cur, drink):
     result = []
-    cur.execute(("SELECT Liquids.name, Mix.amount "
-                 "FROM Liquids JOIN Drinks JOIN MIX "
-                 "WHERE Drinks.name ==  ? AND Drinks.id == Mix.drink_id AND Liquids.id == Mix.liquid_id"), (drink_name,))
+    
+    if str(drink).isdigit():#if drink is int
+        cur.execute(("SELECT Liquids.name, Mix.Amount "
+                     "FROM Liquids JOIN Mix "
+                     "WHERE Mix.drink_id == ? AND Liquids.id == Mix.liquid_id"), (drink,))
+    else:#drink is string
+        cur.execute(("SELECT Liquids.name, Mix.amount "
+                     "FROM Liquids JOIN Drinks JOIN MIX "
+                     "WHERE Drinks.name ==  ? AND Drinks.id == Mix.drink_id AND Liquids.id == Mix.liquid_id"), (drink,))
     for row in cur:
         result.append(row)
     return result
@@ -77,5 +98,14 @@ def list_all_drinks(cur):
         result.append(row)
     return result
 
-
+"""get_drink_id
+   returns(int) : id of drink
+   cur(obj) : db cursor object
+   drink(string) : name of drink
+"""
+def get_drink_id(cur, drink):
+    
+    cur.execute("SELECT id FROM Drinks WHERE name == ?", (drink,))
+    
+    return cur.fetchone()[0]
 
--- drinkBot.js
+++ drinkBot.js
@@ -6,5 +6,7 @@
 $(document).ready(function(){
 
 	$('#drinks').accordion();
-
+	$('button.order').click(function(e){
+		alert('You ordered drink: ' + $(e.target).attr('id')) ;
+	});
 });
